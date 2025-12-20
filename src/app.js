43559d6af63a5b8512efacc9f0974594216ee762
Hamidullah hamidi <hamidullahh754@gmail.com>
2025-12-20 15:24:48 +0430

    add node engine, eslint, prettier, and lint script

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({ origin: '*' }));
 
 app.get('/', (req, res) => {
   res.send('Habit tracker API is running');
