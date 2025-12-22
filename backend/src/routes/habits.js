9a484c82c7d63e9ccc208792be6764cf84837652
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2025-12-23 01:08:35 +0430

    Add post & delete routes for the habitCompletion. #9

--- backend/src/routes/habits.js
+++ backend/src/routes/habits.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import { asyncHandler } from '../../utils/asyncHandler.js';
-import { createHabit } from '../controllers/habitsController.js';
+import {
+  completeHabit,
+  createHabit,
+  uncompleteHabit,
+} from '../controllers/habitsController.js';
 
 const router = express.Router();
 
 router.post('/', asyncHandler(createHabit));
+router.post('/:id/complete', asyncHandler(completeHabit));
+router.delete('/:id/complete', asyncHandler(uncompleteHabit));
 
 export default router;
