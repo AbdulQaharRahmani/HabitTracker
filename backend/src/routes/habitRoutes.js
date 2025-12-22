6ec647feac3e31d58ad1de4f20af22928bdb4d91
Hamidullah hamidi <hamidullahh754@gmail.com>
2025-12-23 02:27:28 +0430

    add route for getHabits and delete .gitKeep files

--- backend/src/routes/habitRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getHabits } from '../controllers/habitsController.js';
-
-const router = express.Router();
-
-router.route('/').get(getHabits);
-
-export default router;
