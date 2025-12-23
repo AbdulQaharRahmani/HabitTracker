28984a3ac0cdc123634110468e7c089e8de8f504
Hamidullah hamidi <hamidullahh754@gmail.com>
2025-12-23 11:28:30 +0430

    Add comment to isOwner function

--- backend/src/models/Habit.js
+++ backend/src/models/Habit.js
@@ -27,6 +27,7 @@ const HabitSchema = new mongoose.Schema(
   }
 );
 
+// Check if the habit belongs to the logged-in user
 HabitSchema.methods.isOwner = function (userId) {
   return String(this.userId) === String(userId);
 };
