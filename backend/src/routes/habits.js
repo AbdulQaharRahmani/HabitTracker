60c8a93432f5570290a4b7a259ae081ad5b23958
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2025-12-24 00:46:55 +0430

    Add '/reorder' route. #24

--- backend/src/routes/habits.js
+++ backend/src/routes/habits.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { asyncHandler } from '../../utils/asyncHandler.js';
-import { createHabit, getHabits } from '../controllers/habitsController.js';
+import {
+  createHabit,
+  getHabits,
+  reorderHabits,
+} from '../controllers/habitsController.js';
 
 const router = express.Router();
 
 router.post('/', asyncHandler(createHabit));
 router.get('/', asyncHandler(getHabits));
-
+router.put('/reorder', asyncHandler(reorderHabits));
 export default router;
