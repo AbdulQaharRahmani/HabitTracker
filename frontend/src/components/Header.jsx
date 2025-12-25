296a3891d4392d659497923b3cae3a5222aa7835
fataneh-mow <fataneh.mow@gmail.com>
2025-12-25 22:52:05 +0430

    Header.jsx component added

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header () {
+    return (
+        <div className="ml-[6rem] mt-[2rem] leading-[18px]">
+            <h1 className="text-5xl text-black my-2">All Habits</h1>
+            <p className="text-gray">Manage and track your daily routines effectively.</p>
+        </div>
+    )
+}
