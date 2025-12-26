cd6d3cd5ff5d77528c46f6aef3632572ff273447
fataneh-mow <fataneh.mow@gmail.com>
2025-12-26 16:17:22 +0430

    App.jsx modified, new componenets added #36

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,46 @@
 import Header from './components/Header.jsx';
 import DarkMode from './components/DarkMode.jsx';
 import Search from './components/Search.jsx';
+import HabitCard from './components/HabitCard.jsx';
 
 function App() {
   return (
-  <div className="grid grid-cols-[240px_1fr] min-h-screen">
-    {/* Sidebar */}
-    <div className="px-[9rem] sidebar-bg">
-      Sidebar goes here
-    </div>
-
-    {/* Main content */}
-    <div className="px-6">
-      <div className="flex items-center justify-between">
-        <Header />
-        <DarkMode/>
+    <div className="grid grid-cols-[240px_1fr] min-h-screen">
+      
+      {/* Sidebar */}
+      <div className="px-6 sidebar-bg">
+        Sidebar goes here
       </div>
 
-      <hr className="my-4 ml-12 mr-4 mx-auto border-gray-200" />
+      {/* Main */}
+      <div className="px-6">
+        
+        {/* Header */}
+        <div className="flex items-center justify-between">
+          <Header />
+          <DarkMode />
+        </div>
+        <hr className="my-4 ml-12 mr-4 border-gray-200" />
 
-      <div className='ml-12 my-6'>
-        <Search />      
+        {/* Search */}
+        <div className="ml-12 my-6">
+          <Search />
+        </div>
+
+        {/* Habit list of Cards*/}
+        <div className="ml-12 mt-6 space-y-4">
+          <HabitCard
+            title="Morning"
+            description="Morning sport"
+            category="Health"
+            time="Daily"
+            duration="30m"
+          />
+        </div>
+
       </div>
-
     </div>
-  </div>
-  )
+  );
 }
 
 export default App;
