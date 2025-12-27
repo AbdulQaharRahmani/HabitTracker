19846cf7c8defd71bffa0429c776139b7843832e
Farhadakbary <168742706+Farhadakbary@users.noreply.github.com>
2025-12-27 16:43:08 +0430

    #15 Add_Habit_Feature_Implementation ui designed

---
+++ mobile/lib/features/add_habit_model.dart
@@ -0,0 +1,12 @@
+class HabitData {
+  final String title;
+  final String description;
+  final String frequency;
+  final String category;
+  HabitData({
+    required this.title,
+    required this.description,
+    required this.frequency,
+    required this.category,
+  });
+}
