92c4c7afd3c62a6e40c4a906aef9ed307731a8e1
fataneh-mow <fataneh.mow@gmail.com>
2025-12-27 23:34:31 +0430

    feat: new AddHabit component added, bugs fixed #36

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Header from './components/Header.jsx';
 import DarkMode from './components/DarkMode.jsx';
 import Search from './components/Search.jsx';
@@ -5,6 +6,12 @@ import HabitCard from './components/HabitCard.jsx';
 import AddHabit from './components/AddHabit.jsx';
 
 function App() {
+  const [habits, setHabits] = useState([]);
+  
+  const handleAddHabit = (newHabit) => {
+    setHabits((prevHabits) => [...prevHabits, newHabit]);
+  };
+  
   return (
     <div className="grid grid-cols-[240px_1fr] min-h-screen">
       
@@ -24,20 +31,16 @@ function App() {
         <hr className="my-4 ml-12 mr-4 border-gray-200" />
 
         {/* Search , View, AddHabit*/}
-        <div className="ml-12 my-6 grid grid-cols-2 ">
+        <div className="ml-12 my-6 grid grid-cols-2 items-center">
           <Search />
-          <AddHabit />
+          <AddHabit onAddHabit={handleAddHabit} />
         </div>
 
-        {/* Habit list of Cards*/}
+        {/* Habit list */}
         <div className="ml-12 mt-6 space-y-4">
-          <HabitCard
-            title="Morning"
-            description="Morning sport"
-            category="Health"
-            time="Daily"
-            duration="30m"
-          />
+          {habits.map((habit) => (
+            <HabitCard key={habit.id} {...habit} />
+          ))}
         </div>
 
       </div>
