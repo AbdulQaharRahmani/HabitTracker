31e4521633d9ce8595b403080cc146bd66d0ed0b
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2025-12-26 20:54:28 -0800

    Appy changes to middelware

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,20 +1,29 @@
 import jwt from 'jsonwebtoken';
 import { AppError } from '../../utils/error.js';
 import { asyncHandler } from '../../utils/asyncHandler.js';
+import { UserModel } from '../models/User.js';
 
-export const authenticationToken = asyncHandler( async(req, res, next) => {  
-    const authHeader = req.headers.authorization;
+export const authenticationToken = asyncHandler(async (req, res, next) => {
+  const authHeader = req.headers.authorization;
 
-    if(!authHeader || !authHeader.startsWith('Bearer')) {
-        throw new AppError('Unauthorized: Token missing ', 401);
-    }
+  if (!authHeader || !authHeader.startsWith('Bearer')) {
+    throw new AppError('Unauthorized: Token missing ', 401);
+  }
 
-    const token = authHeader.split(' ')[1]
-    const decoded = await jwt.verify(token, process.env.JWT_SECRET)
-    if(!decoded.id){
-        throw new AppError('Unauthorized: Invalid token payload', 401)
-    }
+  const token = authHeader.split(' ')[1];
+  const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+  if (!decoded.id) {
+    throw new AppError('Unauthorized: Invalid token payload', 401);
+  }
 
-    req.user = decoded
-    next()
-})
+  const user = await UserModel.findById(decoded.id);
+  if (!user) {
+    throw new AppError('Unauthorized: User not found', 401);
+  }
+
+  req.user = {
+    _id: user._id,
+    email: user.email,
+  };
+  next();
+});
