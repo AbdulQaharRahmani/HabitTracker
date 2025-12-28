63dca2cb240f351b4ca934095a9251dbd54f7fbc
fataneh-mow <fataneh.mow@gmail.com>
2025-12-28 19:15:59 +0430

    feat: View componenet created and functionality added #36

---
+++ frontend/src/components/View.jsx
@@ -0,0 +1,37 @@
+import { FaList } from 'react-icons/fa';
+import { IoGrid } from 'react-icons/io5';
+
+export default function View({ viewMode, setViewMode }) {
+  return (
+    <div className="px-4 py-2 border mx-auto border-gray-100 shadow-sm inline-flex items-center gap-3 bg-white rounded-lg">
+      
+      <span className="text-gray text-lg">
+        View |
+      </span>
+
+      <div className="inline-flex gap-2">
+        <button
+          onClick={() => setViewMode('grid')}
+          className={`p-1 rounded transition ${
+            viewMode === 'grid'
+              ? 'text-indigo-500 bg-indigo-50'
+              : 'text-gray-400 hover:text-gray-600'
+          }`}
+        >
+          <IoGrid size={18} />
+        </button>
+
+        <button
+          onClick={() => setViewMode('list')}
+          className={`p-1 rounded transition ${
+            viewMode === 'list'
+              ? 'text-indigo-500 bg-indigo-50'
+              : 'text-gray-400 hover:text-gray-600'
+          }`}
+        >
+          <FaList size={18} />
+        </button>
+      </div>
+    </div>
+  );
+}
