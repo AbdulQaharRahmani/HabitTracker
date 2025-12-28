3941b159b799205415ce817804a4a4044636f5f6
fataneh-mow <fataneh.mow@gmail.com>
2025-12-28 14:12:20 +0430

    feat: Search.jsx functionality added, serach thorough Habit title #36

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,22 +1,23 @@
 import { FaSearch } from 'react-icons/fa';
 
-export default function Search() {
+export default function Search({ searchTerm, setSearchTerm }) {
   return (
     <div className="w-full max-w-md">
-      <form className="flex items-center border-1 rounded-lg overflow-hidden bg-white shadow-sm">
-        <button
-          type="submit"
+      <div className="flex items-center border-1 rounded-lg overflow-hidden bg-white shadow-sm">
+        <span
           className="p-2 flex items-center justify-center"
         >
           <FaSearch className="text-gray-500 text-lg" />
-        </button>
+        </span>
 
         <input
           type="text"
           placeholder="Search habits..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
           className="flex-1 px-4 py-2 outline-none text-gray"
         />
-      </form>
+      </div>
     </div>
   );
 }
