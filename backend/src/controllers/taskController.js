863e02678ddff5d0734e23cc966e393eed41326c
Hamidullah hamidi <hamidullahh754@gmail.com>
2025-12-29 21:22:43 +0430

    Remove my mongoDB atlas connection string #81

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -5,13 +5,14 @@ export const createTask = async (req, res) => {
   const { title, description, priority, dueDate } = req.body;
 
   if (!title || !description)
-    throw new AppError('Please add title or description', 400);
+    throw new AppError('Please Provide title and description', 400);
 
   const task = await TaskModel.create({
     title,
     description,
     priority,
     dueDate,
+    userId: req.user._id,
   });
 
   res.status(201).json({
