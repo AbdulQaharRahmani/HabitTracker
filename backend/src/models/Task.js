af2f7b9017b37063ebc36a6a28cfc891f1c28d50
Hamidullah hamidi <hamidullahh754@gmail.com>
2025-12-29 19:30:13 +0430

    Create a temporary task model for testing

---
+++ backend/src/models/Task.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['active', 'done'],
+      default: 'active',
+    },
+    priority: {
+      type: String,
+      enum: ['low', 'medium', 'high'],
+      default: 'low',
+    },
+    dueDate: {
+      type: Date,
+      default: null,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    deletedAt: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const TaskModel = mongoose.model('TaskModel', taskSchema);
