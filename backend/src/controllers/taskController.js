7592341fada513623b12db5fab629d20725ac87f
Hamidullah hamidi <hamidullahh754@gmail.com>
2025-12-30 11:33:04 +0430

    Remove the validation for title and description #81

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -1,11 +1,7 @@
 import { TaskModel } from '../models/Task.js';
-import { AppError } from '../utils/error.js';
 
 export const createTask = async (req, res) => {
   const { title, description, priority, dueDate } = req.body;
-
-  if (!title || !description)
-    throw new AppError('Please Provide title and description', 400);
 
   const task = await TaskModel.create({
     title,
