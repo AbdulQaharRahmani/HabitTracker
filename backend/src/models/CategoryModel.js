d68f42285e08836ed4a9472320f08d9894f3cc7f
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2025-12-30 14:45:04 +0430

    Implement Category model. #73

---
+++ backend/src/models/CategoryModel.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const CategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      minLength: 1,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    icon: { type: String, default: '' },
+    backgroundColor: { type: String, default: '' },
+  },
+  { timestamps: true }
+);
+
+CategorySchema.index({ userId: 1, name: 1 }, { unique: true });
+
+export const CategoryModel = mongoose.model('Category', CategorySchema);
