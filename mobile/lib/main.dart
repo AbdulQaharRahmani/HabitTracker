c16cd08c6cec9b414090fb61459b77096d063802
Farhadakbary <168742706+Farhadakbary@users.noreply.github.com>
2025-12-30 23:43:18 +0430

    #20 implement_date_selection_on_today_page designed

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,29 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:habit_tracker/screans/login_screen.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:habit_tracker/screans/sign_up_page.dart';
 
 void main() {
 
   runApp(
-       ScreenUtilInit(
-        designSize: const Size(375, 812),
-        minTextAdapt: true,
-        builder: (context, child) {
-          return const MaterialApp(
-            debugShowCheckedModeBanner: false,
-            home: LoginScreen(),
-          );
-        },
-      ),
+    ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      builder: (context, child) {
+        return const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: SignUpPage(),
+        );
+      },
+    ),
   );
 }
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-         home:SignUpPage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
