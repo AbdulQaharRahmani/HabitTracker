8706e2aab94e70f49c82c3c6cb5cd1897d0e99f9
Hamidullah hamidi <hamidullahh754@gmail.com>
2025-12-30 13:46:14 +0430

    fix: validate req.user in controllers #81

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -1,6 +1,9 @@
 import { TaskModel } from '../models/Task.js';
+import { AppError } from '../utils/error.js';
 
 export const createTask = async (req, res) => {
+  if (!req.user) throw new AppError('User is not authorized.', 401);
+
   const { title, description, priority, dueDate } = req.body;
 
   const task = await TaskModel.create({
