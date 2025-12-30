0ef4d38dd6061921fb32a63d6186f6587b64ffda
fataneh-mow <fataneh.mow@gmail.com>
2025-12-30 21:12:17 +0430

    fix: logic for random color and random icon removed HabitCard icon and color set static #36

--- frontend/src/components/HabitCard.jsx
+++ frontend/src/components/HabitCard.jsx
@@ -1,52 +1,13 @@
-import { FaCircle } from 'react-icons/fa';
+import { FaCheckCircle, FaCircle } from 'react-icons/fa';
 import HabitCardIcon from './HabitCardIcon';
 
-import {
-  FaBiking, FaHeartbeat, FaRunning, FaSwimmer, FaLungs, FaBrain,
-  FaBook, FaBookOpen, FaGraduationCap, FaPenNib, FaPencilAlt, FaLightbulb, FaChalkboardTeacher, FaLaptopCode, FaUniversity,
-  FaPalette, FaMusic,
-  FaCheckCircle, FaTasks, FaClock, FaBolt, FaChartLine,
-  FaDumbbell, FaWeightHanging, FaBicycle
-} from 'react-icons/fa';
-
-const categoriesIcon = {
-  Health: [FaBiking, FaHeartbeat, FaRunning, FaSwimmer, FaLungs, FaBrain],
-  Learning: [FaBook, FaBookOpen, FaGraduationCap, FaPenNib, FaPencilAlt, FaBrain, FaLightbulb, FaChalkboardTeacher, FaLaptopCode, FaUniversity],
-  Creativity: [FaPalette, FaPenNib, FaMusic, FaBrain, FaLightbulb],
-  Productivity: [FaCheckCircle, FaTasks, FaClock, FaBolt, FaChartLine],
-  Fitness: [FaDumbbell, FaWeightHanging, FaBicycle]
-};
-
-const colors = {
-  '#6366F1': '#EEF4FF',
-  '#66C78A': '#F6FEF9',
-  '#0D9488': '#EDFDF6',
-  '#9333EA': '#FBF4FF',
-  '#D97706': '#FFF7EC',
-  '#E53D62': '#FEF1F4'
-};
-
-function hashStringToNumber(str, max) {
-  let hash = 0;
-  for (let i = 0; i < str.length; i++) {
-    hash = (hash < 5) - hash + str.charCodeAt(i);
-    hash = hash & hash;
-  }
-  return Math.abs(hash) % max;
-}
-
 export default function HabitCard({ title, description, category, time, duration }) {
-  const icons = categoriesIcon[category] || categoriesIcon['Health'];
-  const iconIndex = hashStringToNumber(title + category, icons.length); // certain icon
-  const colorKeys = Object.keys(colors);
-  const colorIndex = hashStringToNumber(title + category + 'color', colorKeys.length); // certain color
-
-  const Icon = icons[iconIndex];
-  const color = colorKeys[colorIndex];
-  const bgColor = colors[color];
+  const color = '#6366F1';
+  const bgColor = '#EEF4FF';
+  const Icon = FaCheckCircle;  
 
   return (
-    <div className="mx-auto my-4 p-4 rounded-xl bg-white  shadow-sm flex items-start gap-4">
+    <div className="mx-auto my-4 p-4 rounded-xl bg-white shadow-sm flex items-start gap-4">
       <HabitCardIcon Icon={Icon} color={color} bgColor={bgColor} />
 
       <div>
