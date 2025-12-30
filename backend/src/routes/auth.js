1dafcaf403017c6f95b400633a4cb7ba67cac953
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2025-12-30 10:57:31 -0800

    change auth routes to default

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,20 +1,11 @@
 import express from 'express';
 import { asyncHandler } from '../utils/asyncHandler.js';
 import { loginUser, registerUser } from '../controllers/authController.js';
-import {
-  loginValidator,
-  registerValidator,
-} from '../validators/validateUser.js';
-import { validate } from '../middleware/validate.js';
+import { loginValidate, registerValidate } from '../middleware/validateUser.js';
 
 const router = express.Router();
 
-router.post(
-  '/register',
-  registerValidator,
-  validate,
-  asyncHandler(registerUser)
-);
-router.post('/login', loginValidator, validate, asyncHandler(loginUser));
+router.post('/register', registerValidate, asyncHandler(registerUser));
+router.post('/login', loginValidate, asyncHandler(loginUser));
 
 export default router;
