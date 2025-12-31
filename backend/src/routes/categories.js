48053c609c364e76273ce99f93dcfad3ddd628f0
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2025-12-31 21:54:06 +0430

    Rename files. #73

--- backend/src/routes/categories.js
+++ backend/src/routes/categories.js
@@ -5,9 +5,9 @@ import {
   updateCategory,
   deleteCategory,
 } from '../controllers/categoryController.js';
-import { categoryValidator } from '../validators/categoryValidators.js';
 import { validate } from '../middleware/validate.js';
-import { IdValidator } from '../validators/IdValidator.js';
+import { categoryValidator } from '../validators/validateCategory.js';
+import { IdValidator } from '../validators/validateId.js';
 
 const router = express.Router();
 
