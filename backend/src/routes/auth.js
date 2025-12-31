5ad95d22850dadb4dcf008415d6290206d088cfa
Hamidullah hamidi <hamidullahh754@gmail.com>
2025-12-31 11:43:27 +0430

    fix typo

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import { asyncHandler } from '../utils/asyncHandler.js';
-import { loginUser, registerUser } from '../controllers/authController.js';
+import {
+  getProfilePicture,
+  loginUser,
+  registerUser,
+  uploadProfilePicture,
+} from '../controllers/authController.js';
 import { loginValidate, registerValidate } from '../middleware/validateUser.js';
 
 const router = express.Router();
 
 router.post('/register', registerValidate, asyncHandler(registerUser));
 router.post('/login', loginValidate, asyncHandler(loginUser));
+router.post('/profile-picture', asyncHandler(uploadProfilePicture));
+router.get('/profile-picture', asyncHandler(getProfilePicture));
 
 export default router;
