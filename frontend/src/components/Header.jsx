3b189bd0a121fc3b1f50d0e195ca45d23c6dba33
fataneh-mow <fataneh.mow@gmail.com>
2025-12-31 20:10:17 +0430

    style: margin left decreased #36

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header () {
     return (
-        <div className="ml-[3rem] mt-[1rem] leading-[18px]">
+        <div className="ml-[2rem] mt-[1rem] leading-[18px]">
             <h1 className="text-4xl text-black my-2">All Habits</h1>
             <p className="text-gray">Manage and track your daily routines effectively.</p>
         </div>
