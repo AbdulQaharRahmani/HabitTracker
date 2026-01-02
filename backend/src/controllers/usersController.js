e53863f13cb571202673e6a6585187caa871b48e
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-01-02 17:15:22 +0330

    Edit some comments

---
+++ backend/src/controllers/usersController.js
@@ -0,0 +1,52 @@
+import fs from 'fs';
+import path from 'path';
+import { AppError, notFound } from '../utils/error.js';
+import { UserModel } from '../models/User.js';
+
+//  Upload or update user's profile picture
+export const uploadProfilePicture = async (req, res) => {
+  if (!req.user) throw new AppError('User is not authorized.', 401);
+  if (!req.file) throw new AppError('Please upload a profile image', 400);
+
+  const user = await UserModel.findById(req.user._id);
+  if (!user) throw notFound('User');
+
+  // Delete previous profile picture if exists
+  if (user.profilePicture) {
+    const oldPath = path.join(
+      process.cwd(),
+      'src/uploads/profile',
+      path.basename(user.profilePicture)
+    );
+    if (fs.existsSync(oldPath)) fs.unlinkSync(oldPath);
+  }
+  // console.log(process.cwd()); => D:\HabitTracker\backend
+
+  user.profilePicture = `/uploads/profile/${req.file.filename}`;
+  await user.save();
+
+  res.status(201).json({
+    success: true,
+    message: 'Your profile picture uploaded',
+  });
+};
+
+//  Get user's profile picture
+export const getProfilePicture = async (req, res) => {
+  const user = await UserModel.findById(req.params.id);
+  if (!user) throw notFound('User');
+
+  if (!user.profilePicture)
+    return res.status(200).json({
+      success: true,
+      message: 'User does not have profile picture',
+    });
+
+  res.sendFile(
+    path.join(
+      process.cwd(),
+      '/src/uploads/profile',
+      path.basename(user.profilePicture)
+    )
+  );
+};
---
+++ backend/src/routes/user.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { upload } from '../middleware/upload.js';
+import { asyncHandler } from '../utils/asyncHandler.js';
+import {
+  getProfilePicture,
+  uploadProfilePicture,
+} from '../controllers/authController.js';
+
+const router = express.Router();
+
+router.post(
+  '/profile-picture',
+  upload.single('profilePicture'),
+  asyncHandler(uploadProfilePicture)
+);
+router.get('/:id/profile-picture', asyncHandler(getProfilePicture));
+
+export default router;
