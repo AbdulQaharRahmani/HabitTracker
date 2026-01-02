7c86a1d4a55381b918776f868523b07543b7103a
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-01-02 02:54:42 -0800

    add id validator for route

--- backend/src/routes/task.js
+++ backend/src/routes/task.js
@@ -6,12 +6,13 @@ import {
   updateTaskValidator,
 } from '../validators/validateTask.js';
 import { validate } from '../middleware/validate.js';
+import { IdValidator } from '../validators/validateId.js';
 
 const router = express.Router();
 
 router.route('/').post(createTaskValidator, validate, asyncHandler(createTask));
 router
   .route('/:id')
-  .put(updateTaskValidator, validate, asyncHandler(updateTask));
+  .put(IdValidator, updateTaskValidator, validate, asyncHandler(updateTask));
 
 export default router;
