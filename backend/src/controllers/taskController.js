798308e0eb77027c0be36e05dcc9129d99cfc061
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-01-02 00:41:51 -0800

    Implement updateTask in controller

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -1,5 +1,5 @@
 import { TaskModel } from '../models/Task.js';
-import { AppError } from '../utils/error.js';
+import { AppError, notFound } from '../utils/error.js';
 
 export const createTask = async (req, res) => {
   if (!req.user) throw new AppError('User is not authorized.', 401);
@@ -18,4 +18,29 @@ export const createTask = async (req, res) => {
     success: true,
     data: task,
   });
+};
+
+export const updateTasks = async (req, res) => {
+  if (!req.user) throw new AppError('User is not authorized', 401);
+
+  const task = await TaskModel.findById(req.params.id);
+  if (!task) throw notFound('Task');
+
+  //Ensure the task belongs to the authenticated user
+  if (String(req.user._id) !== String(task.userId))
+    throw new AppError('You are not allowed to update this task', 403);
+
+  const { title, description, status, priority, dueDate } = req.body;
+
+  if(title !== undefined) task.title = title
+  if(description !== undefined) task.description = description
+  if(status !== undefined) task.status = status
+  if(priority !== undefined) task.priority = priority
+  if(dueDate !== undefined) task.dueDate = dueDate
+
+  await task.save()
+  res.status(200).json({
+    success: true,
+    data: task
+  })
 };
