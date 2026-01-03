1369f9b19c41ace4bc4b4d5ee8d4ef0f6ede28ee
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-01-03 23:42:50 +0430

    Add filter route for task. #86

--- backend/src/routes/task.js
+++ backend/src/routes/task.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import { asyncHandler } from '../utils/asyncHandler.js';
-import { createTask } from '../controllers/taskController.js';
+import { createTask, filterTasks } from '../controllers/taskController.js';
+import { filterTasksValidator } from '../validators/validateTask.js';
+import { validate } from '../middleware/validate.js';
 
 const router = express.Router();
 
 router.route('/').post(asyncHandler(createTask));
+router.get(
+  '/filter',
+  filterTasksValidator,
+  validate,
+  asyncHandler(filterTasks)
+);
 
 export default router;
