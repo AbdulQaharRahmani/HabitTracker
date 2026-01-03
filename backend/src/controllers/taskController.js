d5b0503931f6a55b1dce85ae3ed4a6ab6855eeb4
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-01-03 19:35:18 +0430

    Create function that fetch tasks #82

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -19,3 +19,15 @@ export const createTask = async (req, res) => {
     data: task,
   });
 };
+
+// Get task lists with pagination
+export const getTaskLists = async (req, res) => {
+  if (req.user) throw new AppError('User unAuthorized', 400);
+
+  const tasks = await TaskModel.find({ userId: req.user._id });
+
+  res.status(200).json({
+    success: true,
+    data: tasks,
+  });
+};
