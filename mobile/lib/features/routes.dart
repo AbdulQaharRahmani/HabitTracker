4bdf5d53425f179e3a07b05ea1bdfd002adaf798
Farhadakbary <168742706+Farhadakbary@users.noreply.github.com>
2026-01-03 23:57:36 +0430

    Implement profile screen

---
+++ mobile/lib/features/routes.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker/screens/homeScreen/home_screan.dart';
+import 'package:habit_tracker/screens/profileScreen/profile_screen.dart';
+import '../screens/signUpScreen/signup_screen.dart';
+import '../screens/loginScreen/login_screen.dart';
+
+// for my dear friends add the new routes here
+//   Navigator.pushNamed(context,AppRoutes.home);
+
+class AppRoutes {
+  static const splash = '/';
+  static const signup = '/signup';
+  static const login = '/login';
+  static const home = '/home';
+  static const profile = '/profile';
+
+  static final Map<String, WidgetBuilder> routes = {
+    splash: (context) => const SignUpScreen(),
+    signup: (context) => const SignUpScreen(),
+    login: (context) => const LoginScreen(),
+    home: (context) => const HomeScreen(),
+    profile: (context) => const ProfileScreen(),
+  };
+}
