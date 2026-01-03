cdfb37b9d00701c70ae3ccf8c2ced5a888800d16
HossainRezayi <hossainrezayi26.@gmail.com>
2026-01-03 11:26:11 +0430

    Adding date picker in Today component.

--- frontend/src/pages/Today.jsx
+++ frontend/src/pages/Today.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 function Today() {
+  const [startDate, setStartDate] = useState(new Date());
+
   return (
-    <div>Today</div>
-  )
+    <div>
+      <div>Today</div>
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        minDate={new Date()}
+        filterDate={(date) => date.getDay() !== 6 && date.getDay() !== 0}
+      />
+    </div>
+  );
 }
 
-export default Today
+export default Today;
