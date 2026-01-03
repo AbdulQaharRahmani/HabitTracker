18c8de4ebe0e84caa6f6cf54223232ab4c02b4da
Shukria <shukriasul5@gmail.com>
2026-01-03 23:23:27 +0430

    Add some minor improvements to make sure the AdddHabitModal works finds on habits page

--- frontend/src/store/useAddHabitStore.js
+++ frontend/src/store/useAddHabitStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand"
 
 const useAddHabitStore = create((set) => ({
-    isModalOpen: true,
+    isModalOpen: false,
     habitData: {
         title: "",
         description: "",
