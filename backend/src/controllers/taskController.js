1dff9948b96c9ade6ad3ad618c8e9a95142ca9a2
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-01-04 11:23:44 +0430

    comments resolved #82

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -21,10 +21,10 @@ export const createTask = async (req, res) => {
 };
 
 // Get task lists with pagination
-export const getTaskLists = async (req, res) => {
-  if (!req.user) throw new AppError('User unAuthorized', 400);
+export const getTasks = async (req, res) => {
+  if (!req.user) throw new AppError('User is not authorized.', 401);
 
-  const limit = Number(req.query.limit) || 2;
+  const limit = Number(req.query.limit) || 8;
   const page = Number(req.query.page) || 1;
   const skip = (page - 1) * limit;
 
