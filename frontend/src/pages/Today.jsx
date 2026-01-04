f7746b6aa7f3b30b667a11c6384c4b85005ad1d9
unknown <hanifatavassoli@gmail.com>
2026-01-05 04:23:30 +0430

    import and use CircularProgress compo

--- frontend/src/pages/Today.jsx
+++ frontend/src/pages/Today.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import CircularProgress from '../components/CircularProgress'
 function Today() {
   return (
-    <div>Today</div>
+      <div className="px-10">
+        <h1>Today</h1>
+        <div className="grid grid-cols-3 justify-center items-center gap-10">
+        <CircularProgress percent={75}/>
+        <CircularProgress percent={50}/>
+        <CircularProgress percent={20}/>
+        <CircularProgress />
+        </div>
+      </div>
   )
 }
 
