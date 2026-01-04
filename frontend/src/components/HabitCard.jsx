73b945ce0c43c929ac2a5d7c0689126b5352e02b
unknown <Raihana941>
2026-01-04 23:56:29 +0430

    ix: changed time props to frequency for better undrstanding #38

--- frontend/src/components/HabitCard.jsx
+++ frontend/src/components/HabitCard.jsx
@@ -1,22 +1,27 @@
 import { FaCheckCircle, FaCircle } from 'react-icons/fa';
 import HabitCardIcon from './HabitCardIcon';
 
-export default function HabitCard({ title, description, category, time, duration, viewMode }) {
+export default function HabitCard({ title, description, category, frequency, duration, viewMode }) {
   const color = '#6366F1';
   const bgColor = '#EEF4FF';
   const Icon = FaCheckCircle;  
 
   return (
-    <div className={`
+    <div
+      className={`
         mx-auto p-4 rounded-xl bg-white shadow-sm flex items-start gap-4 min-h-[125px]
-        ${viewMode === 'grid' ? 'w-full max-w-xs' : 'w-full'}
+        ${viewMode === "grid" ? "w-full max-w-xs" : "w-full"}
       `}
     >
       <HabitCardIcon Icon={Icon} color={color} bgColor={bgColor} />
 
       <div>
-        <h3 className="text-xl font-semibold text-gray-500">{title ? title : "No Title"}</h3>
-        <p className="text-sm text-gray-400">{description ? description : "No Description"}</p>
+        <h3 className="text-xl font-semibold text-gray-500">
+          {title ? title : "No Title"}
+        </h3>
+        <p className="text-sm text-gray-400">
+          {description ? description : "No Description"}
+        </p>
 
         <div className="flex items-center gap-4 mt-2">
           <span
@@ -27,7 +32,7 @@ export default function HabitCard({ title, description, category, time, duration
           </span>
 
           <span className="flex items-center text-[12px] text-gray-400">
-            {time ? time : "No Time"}
+            {frequency ? frequency : "No frequency"}
             <FaCircle className="mx-2 text-[5px]" />
             {duration ? duration : "No Duration"}
           </span>
