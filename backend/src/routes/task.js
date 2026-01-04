5dfa0d7244bbf5dda82cd67a6d2957fe1cf75658
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-01-04 00:40:58 -0800

    implement delete task

--- backend/src/routes/task.js
+++ backend/src/routes/task.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { asyncHandler } from '../utils/asyncHandler.js';
-import { createTask } from '../controllers/taskController.js';
+import { createTask, deleteTask } from '../controllers/taskController.js';
+import { IdValidator } from '../validators/validateId.js';
+import { validate } from '../middleware/validate.js';
 
 const router = express.Router();
 
 router.route('/').post(asyncHandler(createTask));
+router.route('/:id').delete(IdValidator, validate,asyncHandler(deleteTask));
 
 export default router;
