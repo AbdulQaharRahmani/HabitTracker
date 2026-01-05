1d1635fd36723ef6bb167d00acc50f0506ccd7af
HossainRezayi <hossainrezayi26.@gmail.com>
2026-01-05 12:35:23 +0430

    Styling date selector

--- frontend/src/pages/Today.jsx
+++ frontend/src/pages/Today.jsx
@@ -1,8 +1,28 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import "./Today.css"; // Import your custom CSS file
 
-import "react-datepicker/dist/react-datepicker.css";
+// Custom Header Component
+const CustomHeader = ({
+  date,
+  decreaseMonth,
+  increaseMonth,
+  prevMonthButtonDisabled,
+  nextMonthButtonDisabled,
+}) => (
+  <div className="custom-header">
+    <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+      {"<"}
+    </button>
+    <div className="month-year-label">
+      {date.toLocaleString("default", { month: "long" })} {date.getFullYear()}
+    </div>
+    <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+      {">"}
+    </button>
+  </div>
+);
 
 function Today() {
   const [startDate, setStartDate] = useState(new Date());
@@ -10,12 +30,18 @@ function Today() {
   return (
     <div>
       <div>Today</div>
-      <DatePicker
-        selected={startDate}
-        onChange={(date) => setStartDate(date)}
-        minDate={new Date()}
-        filterDate={(date) => date.getDay() !== 6 && date.getDay() !== 0}
-      />
+      <div className="container">
+        <div></div>
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          inline
+          minDate={new Date()}
+          filterDate={(date) => date.getDay() !== 6 && date.getDay() !== 0}
+          calendarClassName="custom-calendar"
+          renderCustomHeader={CustomHeader}
+        />
+      </div>
     </div>
   );
 }
