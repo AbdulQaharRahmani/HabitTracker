167368124b9395af083c1925228e31835ee24b17
fataneh-mow <fataneh.mow@gmail.com>
2026-01-07 21:32:14 +0430

    fix: passed the props and values to Heaser component in Habits page #143

--- frontend/src/pages/Habits.jsx
+++ frontend/src/pages/Habits.jsx
@@ -11,20 +11,20 @@ export default function Habits () {
   const [viewMode, setViewMode] = useState('list');
 
   return (
-    <div className="md:px-2 lg:px-4 bg-gray-50"> 
+    <div className="md:px-2 lg:px-4 bg-gray-50">
       {/* Header */}
       <div className="flex flex-col gap-4 md:flex-row md:ml-0 md:items-between md:justify-between sm:flex-row sm:ml-0 sm:items-between sm:justify-between">
-        <Header />
+        <Header
+          title={"All Habits"}
+          subtitle={"Manage and track your daily routines effectively."}
+        />
         <DarkMode />
       </div>
       <hr className="my-4 mx-2 md:ml-6 md:mr-4 border-gray-200" />
       {/* Search , View, AddHabit*/}
       <div className="my-6 flex flex-col gap-4 lg:flex-row lg:items-center lg:justify-between lg:ml-6">
         <div className="w-full lg:w-1/2">
-          <Search 
-            searchTerm={searchTerm}
-            setSearchTerm={setSearchTerm}
-          />
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </div>
 
         <div className="flex flex-row justify-between items-center w-full lg:w-auto lg:flex-row lg:justify-end gap-3 md:flex-col sm:flex-col">
@@ -35,9 +35,9 @@ export default function Habits () {
       {/* Habit list */}
       <div
         className={
-          viewMode === 'grid'
-            ? 'grid grid-cols-1 lg:grid-cols-3 lg:m-0 sm:grid-cols-2  md:grid-cols-1 md:ml-12 gap-6 justify-items-start' 
-            : 'my-6 space-y-4 ml-5'
+          viewMode === "grid"
+            ? "grid grid-cols-1 lg:grid-cols-3 lg:m-0 sm:grid-cols-2  md:grid-cols-1 md:ml-12 gap-6 justify-items-start"
+            : "my-6 space-y-4 ml-5"
         }
       >
         <HabitCard
