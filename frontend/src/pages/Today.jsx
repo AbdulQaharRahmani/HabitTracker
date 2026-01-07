2abff406de5027a6686784392f3a0debd26341fd
unknown <hanifatavassoli@gmail.com>
2026-01-07 06:39:29 +0430

    make the container full responsive

--- frontend/src/pages/Today.jsx
+++ frontend/src/pages/Today.jsx
@@ -2,15 +2,25 @@ import React from 'react'
 import CircularProgress from '../components/habits/CircularProgress'
 function Today() {
   return (
-      <div className="px-10">
-        <h1>Today</h1>
-        <div className="grid grid-cols-3 justify-center items-center gap-10">
-        <CircularProgress percent={75}/>
-        <CircularProgress percent={50}/>
-        <CircularProgress percent={20}/>
-        <CircularProgress />
-        </div>
+      <div className="px-4 sm:px-6 md:px-10">
+      <h1 className="text-lg sm:text-xl md:text-2xl font-semibold mb-4">
+        Today
+      </h1>
+
+      <div className="
+        grid
+        grid-cols-1
+        sm:grid-cols-2
+        md:grid-cols-3
+        place-items-center
+        gap-6
+        sm:gap-8
+        md:gap-10
+      ">
+        <CircularProgress percent={50} />
       </div>
+  </div>
+
   )
 }
 
