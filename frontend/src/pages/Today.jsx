90fe13d71508cc7033d52cef27ed30522969bbfa
hanifa tavassoli <hanifatavassoli@gmail.com>
2026-01-07 20:39:20 +0430

    make full responsive the today container

--- frontend/src/pages/Today.jsx
+++ frontend/src/pages/Today.jsx
@@ -1,36 +1,35 @@
-import React from 'react'
-import CircularProgress from '../components/habits/CircularProgress'
 import React, { useState } from "react";
+import CircularProgress from "../components/habits/CircularProgress";
 import CustomDatePicker from "./CustomDatePicker";
 
 function Today() {
   const [startDate, setStartDate] = useState(new Date());
 
   return (
-      <div className="px-4 sm:px-6 md:px-10">
-      <h1 className="text-lg sm:text-xl md:text-2xl font-semibold mb-4">
+    <>
+      <h1 className="text-lg sm:text-xl md:text-2xl font-semibold mb-6">
         Today
       </h1>
 
-      <div className="
-        grid
-        grid-cols-1
-        sm:grid-cols-2
-        md:grid-cols-3
-        place-items-center
-        gap-6
-        sm:gap-8
-        md:gap-10
-      ">
+      <div
+        className="
+    grid
+    grid-cols-1
+    sm:grid-cols-1
+    md:grid-cols-2
+    lg:grid-cols-3
+    gap-4
+    sm:gap-6
+    justify-items-center
+    max-w-4xl
+    mx-auto
+  "
+      >
+        <CustomDatePicker />
         <CircularProgress percent={50} />
-          <CustomDatePicker
-          selectedDate={startDate}
-          onChange={(date) => setStartDate(date)}
-        />
       </div>
-  </div>
-
-  )
+    </>
+  );
 }
 
-export default Today
+export default Today;
