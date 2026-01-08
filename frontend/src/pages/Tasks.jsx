11cf69699350b772109fbe857435f31d91287c7b
fataneh-mow <fataneh.mow@gmail.com>
2026-01-08 13:36:35 +0430

    feat: header part in Tasks page created #143

--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Header from '../components/Header'
+import AddTask from '../components/AddTask'
 
-function Tasks() {
+function Tasks () {
   return (
-    <div>Tasks</div>
-  )
+    <div className="md:px-2 lg:ml-4 bg-gray-50">
+      <div className="flex flex-rows justify-between">
+        <Header
+          title={"Tasks"}
+          subtitle={"Manage your daily goals and todos."}
+        ></Header>
+        <span className='mt-10'>
+          <AddTask></AddTask>
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default Tasks
