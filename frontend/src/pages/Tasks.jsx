74231cea7eee8f0b6857485396bfb493385c0d5f
fataneh-mow <fataneh.mow@gmail.com>
2026-01-08 20:02:26 +0430

    feat: TaskCard component implemented #143

--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import Header from '../components/Header'
 import AddTask from '../components/AddTask'
+import TaskCard from '../components/TaskCard';
 
 function Tasks () {
   return (
-    <div className="md:px-2 lg:ml-4 bg-gray-50">
-      <div className="flex flex-rows justify-between">
+    <div className="md:px-2 lg:ml-4 bg-gray-50 grid grid-cols gap-4">
+      <div className="flex flex-rows justify-between my-2">
         <Header
           title={"Tasks"}
           subtitle={"Manage your daily goals and todos."}
         ></Header>
-        <span className='mt-10'>
+        <span className='mt-10 mr-8'>
           <AddTask></AddTask>
         </span>
+      </div>
+      <div className=''>
+        <TaskCard 
+          title={"Complete project proposal"}
+          deadline={"Yesterday"}
+          category={"HEALTH"}  
+        />
       </div>
     </div>
   );
