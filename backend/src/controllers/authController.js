6b2f69597dbfc3574b354dbfe1e9dbda9ae7dccd
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-01-09 11:16:01 -0800

    Implement redirectGoogleAuth and handleGoogleCallback to controller

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -2,6 +2,15 @@ import { AppError } from '../utils/error.js';
 import { UserModel } from '../models/User.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
+import crypto from 'crypto';
+import { OAuth2Client } from 'google-auth-library';
+
+//Create a Google OAuth2 client for login, token exchange, and verify the user's ID token
+const client = new OAuth2Client(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
 
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
@@ -35,6 +44,91 @@ export const loginUser = async (req, res) => {
   const pswMatch = await bcrypt.compare(password, user.password);
   if (!pswMatch) {
     throw new AppError('Password is not correct', 400);
+  }
+
+  const token = jwt.sign(
+    { id: user._id, email: user.email },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRES_IN }
+  );
+
+  res.status(200).json({
+    success: true,
+    message: 'Login Successfully',
+    data: {
+      token,
+      id: user._id,
+      email: user.email,
+    },
+  });
+};
+
+export const redirectToGoogleAuth = (req, res) => {
+  //Generate Google OAuth2 login URL for user authentication
+  const url = client.generateAuthUrl({
+    access_type: 'offline',
+    scope: ['openid', 'email', 'profile'],
+    prompt: 'consent',
+  });
+
+  //return res.redirect(url)
+  res.status(200).json({
+    success: true,
+    message: 'click to the link for login via google',
+    data: url,
+  });
+};
+
+export const handleGoogleCallback = async (req, res) => {
+  const { code } = req.query;
+
+  //Exchange code for tokens
+  const { tokens } = await client.getToken(code);
+  client.setCredentials(tokens);
+
+  //Verify Google ID token securely
+  const ticket = await client.verifyIdToken({
+    idToken: tokens.id_token,
+    audience: process.env.GOOGLE_CLIENT_ID,
+  });
+
+  const googleUserinfo = ticket.getPayload();
+
+  //Check user by googleId or email
+  let user =
+    (await UserModel.findOne({ googleId: googleUserinfo.sub })) ||
+    (await UserModel.findOne({ email: googleUserinfo.email }));
+
+  if (!user) {
+    //To prevent error assign a randome password for the user
+    const randomPassword = crypto.randomBytes(32).toString('hex');
+    const newUser = await UserModel.create({
+      googleId: googleUserinfo.sub,
+      email: googleUserinfo.email,
+      username: googleUserinfo.name,
+      profilePicture: googleUserinfo.picture,
+      password: randomPassword,
+    });
+    const token = jwt.sign(
+      { id: newUser._id, email: newUser.email },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: 'User registered and login via Google',
+      data: {
+        token,
+        id: newUser._id,
+        email: newUser.email,
+      },
+    });
+  }
+  
+  if (!user.googleId) {
+    user.googleId = googleUserinfo.sub;
+    await user.save();
   }
 
   const token = jwt.sign(
