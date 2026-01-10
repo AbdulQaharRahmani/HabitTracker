e90d45d990809ef6732cb637b8fe6547904b6f73
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-01-10 22:44:48 +0430

    Remove pagination from category

--- backend/src/controllers/categoryController.js
+++ backend/src/controllers/categoryController.js
@@ -58,13 +58,7 @@ export const deleteCategory = async (req, res) => {
 export const getCategories = async (req, res) => {
   if (!req.user) throw new AppError('User is not authorized.', 401);
 
-  const limit = Number(req.query.limit) || 8;
-  const page = Number(req.query.page) || 1;
-  const skip = (page - 1) * limit;
-
-  const categories = await CategoryModel.find({ userId: req.user._id })
-    .skip(skip)
-    .limit(limit);
+  const categories = await CategoryModel.find({ userId: req.user._id });
 
   res.status(200).json({
     success: true,
