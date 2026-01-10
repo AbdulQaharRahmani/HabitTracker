c2e7e5e4e919c881f90d3a03f25528fffe65c75b
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2026-01-10 10:22:04 +0430

    change style

--- frontend/src/components/TodayList.jsx
+++ frontend/src/components/TodayList.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import HabitCard from './HabitCard'
 import useHabitStore from '../store/useHabitStore';
 
 const TodayList = () => {
   const { habits, loading, error, fetchTodayHabits } = useHabitStore();
-
+  const [viewMode, setViewMode] = useState('grid');
   useEffect(() => {
     fetchTodayHabits();
   }, [])
@@ -15,7 +15,12 @@ const TodayList = () => {
   if (error) return <p>{error}</p>;
 
   return (
-    <div>
+    <div
+       className={
+      viewMode === 'grid'
+        ? 'grid grid-cols-1 lg:grid-cols-3 lg:m-0 sm:grid-cols-2  md:grid-cols-1 md:ml-12 gap-6 justify-items-start'
+        : 'my-6 space-y-4 ml-5'
+      }>
       {
       habits.map((habit) => {
       return (
--- frontend/src/pages/Today.jsx
+++ frontend/src/pages/Today.jsx
@@ -1,26 +1,24 @@
 import React, { useState } from "react";
 import CustomDatePicker from "./CustomDatePicker";
-import TodayList from "../components/todayList";
+import TodayList from "../components/TodayList";
+
 
 function Today() {
   const [startDate, setStartDate] = useState(new Date());
-    const [viewMode, setViewMode] = useState('grid');
+
   return (
-    <div>
-      <div className="container">
+    <div className="grid col-span-2">
+      <div>
         <CustomDatePicker
           selectedDate={startDate}
           onChange={(date) => setStartDate(date)}
         />
       </div>
-      <div
-        className={
-         viewMode === 'grid'
-            ? 'grid grid-cols-1 lg:grid-cols-3 lg:m-0 sm:grid-cols-2  md:grid-cols-1 md:ml-12 gap-6 justify-items-start'
-            : 'my-6 space-y-4 ml-5'
-        }>
+      <div className="md:px-2 lg:px-4 bg-gray-50">
+      <div>
         <TodayList/>
       </div>
+    </div>
     </div>
   );
 }
