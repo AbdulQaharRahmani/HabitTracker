0b2cb8c24c8906fd5663bf8cb9e6291b680d2395
fataneh-mow <fataneh.mow@gmail.com>
2026-01-11 19:30:40 +0430

    feat: useTaskCardStore zustand file created for state managment #143

--- frontend/src/store/useTaskCardStore.js
+++ frontend/src/store/useTaskCardStore.js
@@ -1,16 +1,50 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
 export const useTaskCardStore = create((set) => ({
-  tasks: [],
+  tasks: [
+    {
+      id: 1,
+      title: "Complete project proposal",
+      deadline: "Yesterday",
+      category: "Work",
+      done: false,
+    },
+    {
+      id: 2,
+      title: "Buy groceries for the week",
+      deadline: "Today",
+      category: "Personal",
+      done: false,
+    },
+    {
+      id: 3,
+      title: "Schedule dentist appointment",
+      deadline: "Tomorrow",
+      category: "Health",
+      done: false,
+    },
+    {
+      id: 4,
+      title: "Call Mom",
+      deadline: "No date",
+      category: "Family",
+      done: false,
+    },
+  ],
 
-  toggleTaskDone : (id) => {
-    set((state) => {
-        tasks : state.tasks.map((task) => {
-            task.id == id ? 
-            { ...task, done: !task.done } :
-            task
+    toggleTaskDone: (id) =>
+        set((state) => ({
+            tasks: state.tasks.map((task) =>
+                task.id === id ? { ...task, done: !task.done } : task
+            ),
         })
-    })
-  }
-}));
+    ),
 
+    deleteTask: (id) => 
+        set((state) => ({
+            tasks: state.tasks.filter((task) => task.id !== id)
+        }),
+    )
+
+
+}));
