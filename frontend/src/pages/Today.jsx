ee63ce06870e6633263f5a6ad02257ab97d5c0dd
hanifa tavassoli <hanifatavassoli@gmail.com>
2026-01-11 23:12:28 +0430

    Improve overall responsive layout

--- frontend/src/pages/Today.jsx
+++ frontend/src/pages/Today.jsx
@@ -1,34 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import CircularProgress from "../components/habits/CircularProgress";
 import CustomDatePicker from "./CustomDatePicker";
 
 function Today() {
-  const [startDate, setStartDate] = useState(new Date());
-
   return (
-    <>
-      <h1 className="text-lg sm:text-xl md:text-2xl font-semibold mb-6">
-        Today
+    <div className="w-full max-w-7xl mx-auto px-6 md:px-14">
+      <h1 className="text-xl md:text-2xl font-semibold mb-8">
+        Today&apos;s Habits
       </h1>
 
-      <div
-        className="
-    grid
-    grid-cols-1
-    sm:grid-cols-1
-    md:grid-cols-2
-    lg:grid-cols-3
-    gap-4
-    sm:gap-6
-    justify-items-center
-    max-w-4xl
-    mx-auto
-  "
-      >
-        <CustomDatePicker />
-        <CircularProgress percent={50} />
+      <div className="grid grid-cols-1 lg:grid-cols-[2fr_1fr] gap-8">
+        <div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+            {[1, 2, 3, 4].map((i) => (
+              <div
+                key={i}
+                className="h-[170px] bg-white rounded-2xl border border-dashed border-gray-300"
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-10 items-center ">
+          <CustomDatePicker />
+          <CircularProgress percent={75} />
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
