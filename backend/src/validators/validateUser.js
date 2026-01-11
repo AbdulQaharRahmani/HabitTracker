89defb2ad70c565e4bc0f4c8ae32f22e160892e3
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-01-11 11:57:50 +0430

    Create validator for update username #174

--- backend/src/validators/validateUser.js
+++ backend/src/validators/validateUser.js
@@ -38,3 +38,16 @@ export const changePasswordValidator = [
     .withMessage('New password must contain only letters and numbers')
     .trim(),
 ];
+
+export const updateUsernameValidator = [
+  body('username')
+    .notEmpty()
+    .withMessage('Username is required')
+    .isString()
+    .withMessage('Username must be a string')
+    .isLength({ min: 1, max: 30 })
+    .withMessage(
+      'The username length should be greater than 1 and less than 30 letter'
+    )
+    .trim(),
+];
