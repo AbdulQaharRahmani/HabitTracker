b07edb30a343a1fa36326857db3ef771b861d72c
fataneh-mow <fataneh.mow@gmail.com>
2026-01-12 15:24:49 +0430

    feat:translation implemented #143

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -1,14 +1,22 @@
 import { FaRegCircle, FaCircle, FaCheckCircle } from "react-icons/fa";
 import { MdDeleteOutline } from "react-icons/md";
 import { useTaskCardStore } from "../store/useTaskCardStore";
+import { useTranslation } from "react-i18next";
+import i18n from "../utils/i18n";
 
 export default function TaskCard ({title, deadline, category, done, id}) {
   const completeTask = useTaskCardStore((state) => state.completeTask);
   const deleteTask = useTaskCardStore((state) => state.deleteTask);
 
+  const { t } = useTranslation(); 
+
   return (
     <div className="flex bg-white rounded-xl shadow-sm mx-8">
-      <div className="flex items-center justify-between border-r border-gray-300 mx-4 px-4 pr-8 text-center">
+      <div
+        className={`flex items-center justify-between border-gray-300 mx-4 px-4 pr-8 text-center ${
+          i18n.language === "fa" ? "border-l pl-10" : "border-r "
+        }`}
+      >
         <button onClick={() => completeTask(id)}>
           {done ? (
             <FaCheckCircle size={20} className="text-green-400" />
@@ -28,18 +36,18 @@ export default function TaskCard ({title, deadline, category, done, id}) {
               done ? "text-gray-400 line-through" : "text-gray-800"
             }`}
           >
-            {title}
+            {t(title)}
           </div>
 
           <div className="flex flex-rows-2">
             <span className="bg-indigo-100 w-1/1 block rounded-xl mb-2 mx-4 p-2">
               <p className="flex flex-rows gap-2 text-[0.8rem] text-semibold text-indigo-600">
                 <FaCircle size={6} className="mt-2" />
-                Due:
-                <span className="">{deadline}</span>
+                {t("Due")}:
+                <span className="">{t(deadline)}</span>
               </p>
             </span>
-            <span className="py-2 text-[0.8rem] text-gray-300">{category}</span>
+            <span className="py-2 text-[0.8rem] text-gray-300">{t(category)}</span>
           </div>
         </div>
       </div>
