6ccb57aed4162d63be44a78ed3d6cea662709b06
fataneh-mow <fataneh.mow@gmail.com>
2026-01-14 14:27:10 +0430

    feat: Export data componnet created #3161

---
+++ frontend/src/components/ExportData.jsx
@@ -0,0 +1,23 @@
+import { GrAdd } from "react-icons/gr";
+import { useTranslation } from "react-i18next";
+import i18n from "../utils/i18n";
+import { MdDownload } from "react-icons/md";
+
+
+export default function ExportData () {
+  const { t } = useTranslation(); 
+
+  return (
+    <div className="">
+      <button
+        className="bg-white hover:bg-indigo-50 rounded-lg px-4 py-2 flex items-center justify-center shadow-md text-md transition ease-in duration-200 border-2 border-gray-200"
+        type="button"
+      >
+        <span className="mr-2 text-indigo-500">
+          <MdDownload />
+        </span>
+        <span className="text-gray-800">{t("Export Data")}</span>
+      </button>
+    </div>
+  );
+}
