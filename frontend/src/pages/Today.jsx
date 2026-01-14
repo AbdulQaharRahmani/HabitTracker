49e6f7dc98a4b8df1895a83033ed338d738712d1
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2026-01-14 21:42:25 +0430

    fixed styling issues

--- frontend/src/pages/Today.jsx
+++ frontend/src/pages/Today.jsx
@@ -7,32 +7,50 @@ import CircularProgress from "./../components/habits/CircularProgress"
 function Today() {
   const [startDate, setStartDate] = useState(new Date());
   return (
-    <div className="p-8 bg-[#F8F9FD] min-h-screen">
+    <div className="p-8 bg-gray-50 min-h-screen">
       <div className="mb-8 flex justify-between items-start">
         <div>
           <h1 className="text-3xl font-bold text-gray-800">Today's Habits</h1>
           <p className="text-gray-500 mt-1">Track your daily goals and build consistency.</p>
         </div>
         <div className="flex gap-4">
-          <button className="p-2 bg-white rounded-full shadow-sm text-gray-400">🔔</button>
-          <button className="p-2 bg-white rounded-full shadow-sm text-gray-400">🔍</button>
+          <button className="p-2  rounded-full shadow-sm text-gray-50">🔔</button>
+          <button className="p-2  rounded-full shadow-sm text-gray-50">🔍</button>
         </div>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
-        <div className="lg:col-span-8">
-          <TodayList />
-        </div>
-        <div className="lg:col-span-2">
-          <div className=" rounded-[32px] p-6 border border-gray-50">
-             <CustomDatePicker selectedDate={startDate} onChange={setStartDate} />
-          </div>
+    <div className="grid grid-cols-1 lg:grid-cols-[2fr_1fr] gap-8">
           <div>
-            <CircularProgress percent={75} />
+              <TodayList />
           </div>
+        <div className="flex flex-col gap-10 items-center ">
+          <CustomDatePicker />
+          <CircularProgress percent={75} />
         </div>
       </div>
     </div>
   );
 }
+
+
+      // <div className="grid grid-cols-1 lg:grid-cols-[2fr_1fr] gap-8">
+      //   <div>
+      //     <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+      //       {[1, 2, 3, 4].map((i) => (
+      //         <div
+      //           key={i}
+      //           className="h-[170px] bg-white rounded-2xl border border-dashed border-gray-300"
+      //         />
+      //       ))}
+      //     </div>
+      //   </div>
+
+      //   <div className="flex flex-col gap-10 items-center ">
+      //     <CustomDatePicker />
+      //     <CircularProgress percent={75} />
+      //   </div>
+      // </div>
+
+
+
 
 export default Today;
