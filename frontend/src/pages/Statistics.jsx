9d5582b5fb81029c8b6cc02e6c1567a4ed07f47f
fataneh-mow <fataneh.mow@gmail.com>
2026-01-14 14:28:30 +0430

    feat: header and export data componnets implemented #161

--- frontend/src/pages/Statistics.jsx
+++ frontend/src/pages/Statistics.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import DayilyConsistency from '../components/DailyConsistency'
+import Header from '../components/Header'
+import { MdDownload } from "react-icons/md";
+import ExportData from '../components/ExportData';
+
 
 function Statistics() {
   return (
-    <div>
-       <DayilyConsistency/>
+    <div className=''>
+      <div className="grid grid-cols-2 justify-between my-2 items-center mr-9">
+        <Header
+          title={"Your Progress"}
+          subtitle={"Overview of your consistency and growth."}
+        />
+        <span className='pt-6 ml-auto'>
+          <ExportData />
+        </span>
+      </div>
+
+      <DayilyConsistency />
     </div>
-  )
+  );
 }
 
 export default Statistics
