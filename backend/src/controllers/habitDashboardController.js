e04f6a83a6123fe491926b736c48fb7508225d4a
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-01-14 22:06:22 +0430

    try another way

--- backend/src/controllers/habitDashboardController.js
+++ backend/src/controllers/habitDashboardController.js
@@ -15,16 +15,16 @@ export const getHabitsDashboard = async (req, res) => {
   let today = dayjs();
   let currentDay = today.toDate();
 
+  const activeHabitIds = (
+    await HabitModel.find({ userId: req.user._id, isDeleted: false }).select(
+      '_id'
+    )
+  ).map((h) => h._id);
+  // console.log(activeHabitIds);
+
   while (true) {
     const startOfDay = dayjs(currentDay).startOf('day').toDate();
     const endOfDay = dayjs(currentDay).endOf('day').toDate();
-
-    const activeHabitIds = (
-      await HabitModel.find({ userId: req.user._id, isDeleted: false }).select(
-        '_id'
-      )
-    ).map((h) => h._id);
-    console.log(activeHabitIds);
 
     const exist = await HabitCompletionModel.exists({
       userId: req.user._id,
@@ -45,7 +45,7 @@ export const getHabitsDashboard = async (req, res) => {
   const startOfWeek = today.startOf('week').toDate();
   const endOfWeek = today.endOf('week').toDate();
 
-  const result = await HabitModel.aggregate([
+  const habits = await HabitModel.aggregate([
     { $match: { userId: req.user._id, isDeleted: false } },
     {
       $lookup: {
@@ -73,15 +73,66 @@ export const getHabitsDashboard = async (req, res) => {
     },
   ]);
 
-  // console.log(result);
-  res.status(200).json(result);
+  /*
+  let sum = 0;
 
-  // res.status(200).json({
-  //   success: true,
-  //   data: {
-  //     totalHabits,
-  //     currentStreak,
-  //     completionRate,
-  //   },
-  // });
+  result.map((res) => {
+    switch (res.frequency) {
+      case 'daily':
+        sum += res.CompletedHabits.length / 7;
+        // console.log(sum);
+        break;
+      case 'every-other-day':
+        sum += res.CompletedHabits.length / 3;
+        // console.log(sum);
+        break;
+      case 'weekly':
+        sum += res.CompletedHabits.length / 1;
+        // console.log(sum);
+        break;
+      case 'biweekly':
+        sum += res.CompletedHabits.length / 0.5;
+        // console.log(sum);
+        break;
+      default:
+        return;
+    }
+  });
+
+  console.log(Math.round(sum));
+  // console.log(result.length);
+  res.status(200).json(result);
+*/
+  /*
+  let totalExpectedCompletions = 0;
+  let totalActualCompletions = 0;
+
+  const frequencyMap = {
+    daily: 7,
+    'every-other-day': 3,
+    weekly: 1,
+    biweekly: 2,
+  };
+
+  habits.forEach((habit) => {
+    const expected = frequencyMap[habit.frequency] || 0;
+    const actual = habit.CompletedHabits.length;
+
+    totalExpectedCompletions += expected;
+    totalActualCompletions += Math.min(actual, expected);
+  });
+
+  const completionRate =
+    totalExpectedCompletions === 0
+      ? 0
+      : Math.round((totalActualCompletions / totalExpectedCompletions) * 100);
+*/
+  res.status(200).json({
+    success: true,
+    data: {
+      totalHabits,
+      currentStreak,
+      completionRate,
+    },
+  });
 };
