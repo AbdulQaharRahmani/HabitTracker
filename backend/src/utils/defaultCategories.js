454f8cfdbeeab712cbbd6f1ea63e2ff095af7ead
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-01-16 15:17:00 +0430

    Update defaultCategories. #198

--- backend/src/utils/defaultCategories.js
+++ backend/src/utils/defaultCategories.js
@@ -1,5 +1,5 @@
 const defaultCategories = [
-  { name: 'other', icon: 'other', backgroundColor: '#ed6f07' },
+  { name: 'sport', icon: 'sport', backgroundColor: '#ed6f07' },
   { name: 'health', icon: 'health', backgroundColor: '#82ed18' },
   { name: 'study', icon: 'study', backgroundColor: '#169ef2' },
   { name: 'work', icon: 'work', backgroundColor: '#ed8907' },
