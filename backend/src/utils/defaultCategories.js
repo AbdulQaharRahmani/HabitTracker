61ef6ec0f78307ccb0816871bd802cceebca9924
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-01-16 15:04:00 +0430

    Implement getDefaultCategories function. #198

---
+++ backend/src/utils/defaultCategories.js
@@ -0,0 +1,19 @@
+const defaultCategories = [
+  { name: 'other', icon: 'other', backgroundColor: '#ed6f07' },
+  { name: 'health', icon: 'health', backgroundColor: '#82ed18' },
+  { name: 'study', icon: 'study', backgroundColor: '#169ef2' },
+  { name: 'work', icon: 'work', backgroundColor: '#ed8907' },
+];
+
+export function getDefaultCategories(userId) {
+  if (!userId) return null;
+
+  const categories = [];
+
+  defaultCategories.forEach((category) => {
+    category.userId = userId;
+    categories.push(category);
+  });
+
+  return categories;
+}
