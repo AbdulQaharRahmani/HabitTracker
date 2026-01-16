61e6c7d3dd8671afdc699d5f73d3cf42d9626b97
fataneh-mow <fataneh.mow@gmail.com>
2026-01-16 20:36:44 +0430

    feat: components implemented #161

--- frontend/src/pages/Statistics.jsx
+++ frontend/src/pages/Statistics.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
 import DayilyConsistency from '../components/DailyConsistency'
 import Header from '../components/Header'
 import ExportData from '../components/ExportData';
 import TotalHabitsStatics from '../components/TotalHabitsStatics';
+import CurrentStreakStatics from '../components/CurrentStreakStatics';
+import CompletionRateStatics from '../components/CompletionRateStatics';
+import useHabitStore from "../store/useHabitStore";
 
 function Statistics() {
+  const habits = useHabitStore((state) => state.habits);
+  const totalHabits = habits.length;
+
   return (
     <div className="">
       <div className="grid grid-cols-2 justify-between my-2 items-center mr-9">
@@ -17,10 +22,12 @@ function Statistics() {
         </span>
       </div>
 
-      <div className='my-4'>
-        <TotalHabitsStatics></TotalHabitsStatics>
+      <div className="my-4 flex items-center gap-6 ml-7 mr-9">
+        <TotalHabitsStatics totalHabits={totalHabits}/>
+        <CurrentStreakStatics currentStreak='5' />
+        <CompletionRateStatics completionRate='75'/>
       </div>
-      <div>
+      <div className='mx-7'>
         <DayilyConsistency />
       </div>
     </div>
