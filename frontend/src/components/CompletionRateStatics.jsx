7e3a26e3d50fea8323b7a08e7ae6622868e7f39e
fataneh-mow <fataneh.mow@gmail.com>
2026-01-17 00:39:55 +0430

    feat: translation added and style improved #161

--- frontend/src/components/CompletionRateStatics.jsx
+++ frontend/src/components/CompletionRateStatics.jsx
@@ -1,12 +1,19 @@
 import { PiCircleDashedBold } from "react-icons/pi";
+import i18n from "../utils/i18n";
+import { useTranslation } from "react-i18next";
 
 export default function CompletionRateStatics({ completionRate }) {
+  const { t } = useTranslation();
+  const isRTL = i18n.language === "fa";
+
   return (
-    <div className="bg-white w-1/3 p-4 min-h-[200px] min-w-[200px] rounded-2xl shadow-sm border border-gray-100 relative group overflow-hidden ">
+    <div className="bg-white w-1/3 p-4 min-h-[200px] min-w-[200px] rounded-2xl shadow-sm border border-gray-100 relative group overflow-hidden md:min-w-[170px] xxs:w-full">
       <svg
         fill="currentColor"
         viewBox="0 0 256 256"
-        className="absolute right-4 w-32 h-32 text-green-200 opacity-20"
+        className={`absolute w-32 h-32 text-green-200 opacity-20 ${
+          isRTL ? "left-4 scale-x-[-1]" : "right-4"
+        }`}
       >
         <path d="M92.38,38.05A12,12,0,0,1,101,23.42a108,108,0,0,1,54,0,12,12,0,1,1-6,23.23,84.11,84.11,0,0,0-42,0A12,12,0,0,1,92.38,38.05ZM50.94,52.34a108.1,108.1,0,0,0-27,46.76,12,12,0,0,0,8.37,14.77,12.2,12.2,0,0,0,3.2.43,12,12,0,0,0,11.56-8.8,84,84,0,0,1,21-36.35A12,12,0,1,0,50.94,52.34Zm-3.88,98.14a12,12,0,0,0-23.12,6.42,108,108,0,0,0,27,46.78A12,12,0,0,0,68,186.85,84,84,0,0,1,47.06,150.48ZM149,209.35a84,84,0,0,1-42,0,12,12,0,1,0-6,23.23,108,108,0,0,0,54,0,12,12,0,1,0-6-23.23Zm74.72-67.22A12,12,0,0,0,209,150.5a84,84,0,0,1-21,36.35,12,12,0,0,0,17.12,16.82,108.19,108.19,0,0,0,27-46.77A12,12,0,0,0,223.71,142.13Zm-14.77-36.61a12,12,0,0,0,23.12-6.42,108,108,0,0,0-27-46.78A12,12,0,1,0,188,69.15,84,84,0,0,1,208.94,105.52Z"></path>
       </svg>
@@ -17,7 +24,9 @@ export default function CompletionRateStatics({ completionRate }) {
             size={20}
             className="text-green-500 bg-green-100 w-10 h-12 p-2 rounded-md"
           />
-          <p className="text-gray-400 text-lg uppercase">COMPLETION RATE</p>
+          <p className="text-gray-400 text-lg uppercase">
+            {t("COMPLETION RATE")}
+          </p>
         </span>
         <div className="text-gray-800 font-bold text-[2.5rem] mt-4">
           {completionRate}%
