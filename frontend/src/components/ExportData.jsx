eb49014fe6b8779c8bd3a4be5078e0897fdef6e6
fataneh-mow <fataneh.mow@gmail.com>
2026-01-17 00:40:32 +0430

    feat: translation added

--- frontend/src/components/ExportData.jsx
+++ frontend/src/components/ExportData.jsx
@@ -1,8 +1,6 @@
-import { GrAdd } from "react-icons/gr";
+import { MdDownload } from "react-icons/md";
 import { useTranslation } from "react-i18next";
 import i18n from "../utils/i18n";
-import { MdDownload } from "react-icons/md";
-
 
 export default function ExportData () {
   const { t } = useTranslation(); 
@@ -13,7 +11,9 @@ export default function ExportData () {
         className="bg-white hover:bg-indigo-50 rounded-lg px-4 py-2 flex items-center justify-center shadow-md text-md transition ease-in duration-200 border-2 border-gray-200"
         type="button"
       >
-        <span className="mr-2 text-indigo-500">
+        <span className={`text-indigo-500 ${
+          i18n.language === "fa" ? "ml-2" : "mr-2"
+        }`}>
           <MdDownload />
         </span>
         <span className="text-gray-800">{t("Export Data")}</span>
