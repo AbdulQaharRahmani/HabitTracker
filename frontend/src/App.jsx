d169d16f41ec663f07a9341ae7ceca5c95028f23
Shukria <shukriasul5@gmail.com>
2026-01-17 14:13:18 +0430

    Implement protection for frontend routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,6 +16,7 @@ import Signup from "./pages/Signup";
 import LanguageSwitcher from "./components/internationalization";
 import { Toaster } from "react-hot-toast";
 import "./styles/toast.css";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -44,6 +45,7 @@ function App() {
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route element={<ProtectedRoute />}>
         <Route
           element={
             <Sidebar>
@@ -56,6 +58,7 @@ function App() {
           <Route path="/tasks" element={<Tasks />} />
           <Route path="/statistics" element={<Statistics />} />
           <Route path="/settings" element={<Settings />} />
+        </Route>
         </Route>
         <Route path="*" element={<>not found</>} />
       </Routes>
