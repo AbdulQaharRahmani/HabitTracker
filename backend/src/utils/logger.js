82fda9ae40c0c102435ebd7360732117ff575e94
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-01-21 00:09:53 +0430

    Fix logger for creating error.log file #220

--- backend/src/utils/logger.js
+++ backend/src/utils/logger.js
@@ -1,10 +1,13 @@
 import winston from 'winston';
 
+// Create a centralized logger instance for the application
+
 export const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
+  //  Storage location
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: 'logs/app.log' }),
+    new winston.transports.File({ filename: 'logs/app.log' }), // all logs
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }), // errors logs
   ],
 });
