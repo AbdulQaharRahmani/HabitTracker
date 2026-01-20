b312e2d055fd04a679c3470e24aae6c0820222e4
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-01-20 22:20:20 +0430

    Implement request logging #220

--- backend/src/middleware/logger.js
+++ backend/src/middleware/logger.js
@@ -1,1 +1,17 @@
 import { logger } from '../utils/logger.js';
+
+export const requestLogger = (req, res, next) => {
+  const start = new Date();
+
+  res.on('finish', () => {
+    logger.info('Request completed', {
+      method: req.method,
+      path: req.originalUrl,
+      statusCode: res.statusCode,
+      duration: new Date() - start,
+      userId: req.user?._id,
+    });
+  });
+
+  next();
+};
