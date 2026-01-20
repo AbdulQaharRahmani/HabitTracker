d2be65f93079889db2d018ee5e623d872f8d7557
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2026-01-20 15:52:46 +0430

    resolved duplicate problem

--- frontend/src/components/HabitList.jsx
+++ frontend/src/components/HabitList.jsx
@@ -63,31 +63,13 @@ export default function HabitList({ viewMode }) {
             ))
           )}
         </div>
-        {/* <Pagination
+        <Pagination
           currentPage={page}
           totalCount={allhabits.length}
           siblingCount
           pageSize={ITEMS_PER_PAGE}
           onPageChange={setPage}
-        /> */}
-        {allhabits.length === 0 ? (
-          <p className="text-gray-500 text-lg">
-            You have no habits yet. Add your first habit.
-          </p>
-        ) : (
-          allhabits.map((habit) => (
-            <HabitCard
-              key={habit._id}
-              _id={habit._id}
-              viewMode={viewMode}
-              title={habit.title}
-              description={habit.description}
-              categoryId={habit.categoryId}
-              frequency={habit.frequency}
-              duration={habit.duration}
-            />
-          ))
-        )}
+        />
       </>
     );
   }
