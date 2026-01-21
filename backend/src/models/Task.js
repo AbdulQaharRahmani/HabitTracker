2aeaa779ee9c2b63548917bf0e16cb99a658e5cd
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-01-21 11:22:56 +0430

    comments resolved #220

--- backend/src/models/Task.js
+++ backend/src/models/Task.js
@@ -48,9 +48,12 @@ const taskSchema = new mongoose.Schema(
   }
 );
 
+taskSchema.index({ userId: 1, isDeleted: false });
+
 taskSchema.index(
   { title: 1, userId: 1 },
-  { unique: true, partialFilterExpression: { deletedAt: null } }
+  { unique: true, partialFilterExpression: { isDeleted: false } }
 );
+// partialFilterExpression => Create a unique index only for non-deleted tasks
 
 export const TaskModel = mongoose.model('Task', taskSchema);
