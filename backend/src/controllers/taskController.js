79b20dd921cc622b9ccaf451d1be7249bcbb9758
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-01-25 14:15:09 +0430

    Add sorting in query. #236

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -44,13 +44,22 @@ export const getTasks = async (req, res) => {
   const page = Number(req.query.page) || 1;
   const skip = (page - 1) * limit;
 
+  const tasksTotal = await TaskModel.countDocuments({
+    userId: req.user._id,
+    isDeleted: false,
+  });
+
+  const totalPages = Math.ceil(tasksTotal / limit);
+
   const tasks = await TaskModel.find({ userId: req.user._id, deletedAt: null })
+    .sort({ createdAt: -1 }) // Sort from latest to oldest
     .skip(skip)
     .limit(limit)
     .populate('categoryId', 'name icon backgroundColor');
 
   res.status(200).json({
     success: true,
+    totalPages,
     data: tasks,
   });
 };
