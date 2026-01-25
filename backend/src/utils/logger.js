1e4e671cebdf3cd3a0f3b6d3547ea5e3b56d3969
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-01-25 22:34:11 +0430

    Remove file log storation becaause we store in DB #220

--- backend/src/utils/logger.js
+++ backend/src/utils/logger.js
@@ -1,13 +1,10 @@
 import winston from 'winston';
 
 // Create a centralized logger instance for the application
-
-export const logger = winston.createLogger({
+const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
-  //  Storage location
-  transports: [
-    new winston.transports.File({ filename: 'logs/app.log' }), // all logs
-    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }), // errors logs
-  ],
+  transports: [new winston.transports.Console()],
 });
+
+export default logger;
