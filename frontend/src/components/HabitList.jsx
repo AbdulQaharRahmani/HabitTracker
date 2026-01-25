1c22334fda109dfc648a01dc6f7a31478fe81b71
hanifa tavassoli <hanifatavassoli@gmail.com>
2026-01-25 22:45:02 +0430

    resovle the HabitList conflicts

--- frontend/src/components/HabitList.jsx
+++ frontend/src/components/HabitList.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import HabitCard from "./HabitCard";
 import Pagination from "./Pagination";
 import useHabitStore from "../store/useHabitStore";
+import api from "../../services/api";
+import Pagination from "./Pagination";
 
 export default function HabitList({ viewMode }) {
   const { allhabits, loading, error, fetchHabits } = useHabitStore();
