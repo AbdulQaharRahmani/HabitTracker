2e82e520f3a2a6c29cc77511bf63dc9d17956060
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-01-27 23:45:11 +0430

    Add log-satats route #220

--- backend/src/routes/log.js
+++ backend/src/routes/log.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { asyncHandler } from '../utils/asyncHandler.js';
-import { getLogs } from '../controllers/logController.js';
+import { getLogStats, getLogs } from '../controllers/logController.js';
 
 const router = express.Router();
 
-router.get('/logs', asyncHandler(getLogs));
+router.get('/', asyncHandler(getLogs));
+router.get('/log-stats', asyncHandler(getLogStats));
 
 export default router;
