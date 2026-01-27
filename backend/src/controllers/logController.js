bda65d847d6c1a2d4fcab20e94b32f2d2180bc9f
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-01-27 23:46:53 +0430

    Create two getLog, getLogStats functions #220

--- backend/src/controllers/logController.js
+++ backend/src/controllers/logController.js
@@ -1,17 +1,51 @@
-import { logModel } from '../models/Log.js';
+import { DateHelper } from '../utils/date.js';
+import { getDatesBetween, readLogsByDate } from '../utils/log.js';
 
-export const createLog = async ({ level, message, metadata }) => {
-  await logModel.create({
-    level,
-    message,
-    metadata,
-  });
-};
 export const getLogs = async (req, res) => {
-  const logs = await logModel.find().sort({ timestamp: -1 }).limit(100);
+  const [startDate, endDate] = DateHelper.getStartAndEndOfDate(
+    req.query.startDate,
+    req.query.endDate
+  );
+
+  // return array of all dates between start and end
+  const dates = getDatesBetween(startDate, endDate);
+
+  let logs = [];
+
+  for (const date of dates) {
+    logs.push(...readLogsByDate(date));
+  }
 
   res.status(200).json({
     success: true,
+    count: logs.length,
     data: logs,
+  });
+};
+
+export const getLogStats = async (req, res) => {
+  const [startDate, endDate] = DateHelper.getStartAndEndOfDate(
+    req.query.startDate,
+    req.query.endDate
+  );
+
+  const dates = getDatesBetween(startDate, endDate);
+
+  let logs = [];
+
+  for (const date of dates) {
+    logs.push(...readLogsByDate(date));
+  }
+
+  const stats = {
+    total: logs.length,
+    error: logs.filter((l) => l.level === 'error').length,
+    warn: logs.filter((l) => l.level === 'warn').length,
+    info: logs.filter((l) => l.level === 'info').length,
+  };
+
+  res.status(200).json({
+    success: true,
+    data: stats,
   });
 };
