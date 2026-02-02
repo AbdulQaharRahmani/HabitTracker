b1d99d94fb3c4ff65fa2492971afea1fd4f2cfaf
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-02-02 14:36:13 +0430

    Add log routes to App.jsx #247

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,70 +1,68 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Outlet,
-} from "react-router-dom";
-import "./index.css";
-import Habits from "./pages/Habits";
-import Tasks from "./pages/Tasks";
-import Statistics from "./pages/Statistics";
-import Settings from "./pages/Settings";
-import Sidebar from "./components/Sidebar";
-import Today from "./pages/Today";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import LanguageSwitcher from "./components/internationalization";
-import DarkMode from "./components/DarkMode";
-import { Toaster } from "react-hot-toast";
-import "./styles/toast.css";
-import ProtectedRoute from "./components/ProtectedRoute";
+import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
+import './index.css';
+import Habits from './pages/Habits';
+import Tasks from './pages/Tasks';
+import Statistics from './pages/Statistics';
+import Settings from './pages/Settings';
+import Sidebar from './components/Sidebar';
+import Today from './pages/Today';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import LanguageSwitcher from './components/internationalization';
+import DarkMode from './components/DarkMode';
+import { Toaster } from 'react-hot-toast';
+import './styles/toast.css';
+import ProtectedRoute from './components/ProtectedRoute';
+import RightSidebar from './RightSidebar/RightSidebar';
 
 function App() {
   return (
     <>
       <Toaster
-      position="top-center"
-      reverseOrder={false}
-      gutter={20}
-      containerClassName="toast-container"
-      toastOptions={{
-        duration: 5000,
-        className: "toast-base",
-        success: {
-          className: "toast-success",
-        },
-        error: {
-          className: "toast-error",
-        },
-        loading: {
-          className: "toast-loading",
-        },
-      }}
-    />
-    <Router>
-      <LanguageSwitcher />
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route element={<ProtectedRoute />}>
-        <Route
-          element={
-            <Sidebar>
-              <Outlet />
-            </Sidebar>
-          }
-        >
-          <Route path="/" element={<Today />} />
-          <Route path="/habits" element={<Habits />} />
-          <Route path="/tasks" element={<Tasks />} />
-          <Route path="/statistics" element={<Statistics />} />
-          <Route path="/settings" element={<Settings />} />
-        </Route>
-        </Route>
-        <Route path="*" element={<>not found</>} />
-      </Routes>
-    </Router>
-          </>
+        position='top-center'
+        reverseOrder={false}
+        gutter={20}
+        containerClassName='toast-container'
+        toastOptions={{
+          duration: 5000,
+          className: 'toast-base',
+          success: {
+            className: 'toast-success',
+          },
+          error: {
+            className: 'toast-error',
+          },
+          loading: {
+            className: 'toast-loading',
+          },
+        }}
+      />
+      <Router>
+        <LanguageSwitcher />
+        <Routes>
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
+          <Route element={<ProtectedRoute />}>
+            <Route
+              element={
+                <Sidebar>
+                  <Outlet />
+                </Sidebar>
+              }
+            >
+              <Route path='/' element={<Today />} />
+              <Route path='/habits' element={<Habits />} />
+              <Route path='/tasks' element={<Tasks />} />
+              <Route path='/statistics' element={<Statistics />} />
+              <Route path='/settings' element={<Settings />} />
+            </Route>
+          </Route>
+          <Route path='/logs' element={<RightSidebar />} />
+
+          <Route path='*' element={<>not found</>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
