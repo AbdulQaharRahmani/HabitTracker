374c0187f0363a0c0268c544629a2b10932561b9
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2026-02-10 17:43:03 +0430

    Update rate limiter logi. #296

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,24 +12,15 @@ import syncRoutes from './routes/sync.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { authMiddleware } from './middleware/authMiddleware.js';
 import helmet from 'helmet';
-import rateLimit, { ipKeyGenerator } from 'express-rate-limit';
 import mongoSanitize from '@exortek/express-mongo-sanitize';
 import { logMiddleware } from './middleware/logger.js';
+import { privateLimiter, publicLimiter } from './middleware/rateLimiter.js';
 
 const app = express();
 
 //#region Normal Midlleware
 
 app.use(express.json());
-
-const limiter = rateLimit({
-  keyGenerator: (req) => {
-    return req.user ? req.user._id.toString() : ipKeyGenerator(req);
-  },
-  windowMs: 15 * 60 * 1000,
-  limit: (req) => (req.user ? 100 : 50),
-  message: 'Too many requests, please try again later.',
-});
 
 app.use(
   cors({
@@ -54,16 +45,16 @@ app.get('/api/health', (req, res) => {
 
 // Public routes
 
-app.use('/api/auth', limiter, authRoutes);
+app.use('/api/auth', publicLimiter, authRoutes);
 
 // Protect routes
 
-app.use('/api/categories', authMiddleware, limiter, categoryRoutes);
-app.use('/api/habits', authMiddleware, limiter, habitRoutes);
-app.use('/api/tasks', authMiddleware, limiter, taskRoutes);
-app.use('/api/users', authMiddleware, limiter, userRoutes);
-app.use('/api/offline-data', authMiddleware, limiter, syncRoutes);
-app.use('/api/logs', authMiddleware, limiter, logRoutes);
+app.use('/api/categories', authMiddleware, privateLimiter, categoryRoutes);
+app.use('/api/habits', authMiddleware, privateLimiter, habitRoutes);
+app.use('/api/tasks', authMiddleware, privateLimiter, taskRoutes);
+app.use('/api/users', authMiddleware, privateLimiter, userRoutes);
+app.use('/api/offline-data', authMiddleware, privateLimiter, syncRoutes);
+app.use('/api/logs', authMiddleware, privateLimiter, logRoutes);
 
 //#endregion
 
