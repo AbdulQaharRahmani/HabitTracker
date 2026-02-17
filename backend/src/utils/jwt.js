965e841e833ee5831f6211307cc093a1fe9eec3b
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-02-18 03:15:08 +0430

    add constant error

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -11,6 +11,6 @@ export const generateRefreshToken = () => {
   return crypto.randomBytes(40).toString('hex');
 };
 
-export const hashRefreshToken = (token) => {
+export const hashToken = (token) => {
   return crypto.createHash('sha256').update(token).digest('hex');
 };
