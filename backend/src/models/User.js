e5a56a73c593cee7c7d65a5de58a6468cc3692d5
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-02-18 03:16:27 +0430

    add create reset token function in user model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import crypto from 'crypto';
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -31,6 +32,14 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     default: null,
   },
+  passwordResetToken: {
+    type: String,
+    default: null,
+  },
+  passwordResetExpires: {
+    type: Date,
+    default: null,
+  },
 });
 
 UserSchema.index(
@@ -43,4 +52,16 @@ UserSchema.index(
   }
 );
 
+UserSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
 export const UserModel = mongoose.model('User', UserSchema);
