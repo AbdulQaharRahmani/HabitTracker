92009202a51ced547bfd57a4e35352772d8392ff
Hanifa Tavassoli <hanifatavassoli@gmail.com>
2026-02-23 23:47:19 +0430

    set document direction on load  and langauge change

--- frontend/src/utils/i18n.jsx
+++ frontend/src/utils/i18n.jsx
@@ -24,5 +24,10 @@ i18n.use(initReactI18next).init({
   },
 });
 
+document.documentElement.dir=i18n.dir(i18n.language);
+
+i18n.on('languageChanged',(lng)=>{
+document.documentElment.dir=i18n.dir(lng);
+})
 
 export default i18n;
