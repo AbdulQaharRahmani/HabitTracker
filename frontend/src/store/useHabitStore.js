b2aa14e767b21aae837c10c2d8b62c5f1321228b
Shukria <shukriasul5@gmail.com>
2026-02-24 09:24:21 +0430

    feat: allow user to complete past 7 days habits and see future habits

--- frontend/src/store/useHabitStore.js
+++ frontend/src/store/useHabitStore.js
@@ -111,12 +111,6 @@ const useHabitStore = create((set, get) => ({
     }
   },
   toggleHabit: async (id) => {
-    const formatDate = (date) =>
-      date.toISOString().split("T")[0];
-    if (get().selectedDate.toDateString() !== new Date().toDateString()) {
-      return toast.error("You can only mark today's habit as completed or uncompleted")
-    }
-
     const habitToToggle = get().habits.find(h => h._id === id)
     if (!habitToToggle) return
 
@@ -139,7 +133,7 @@ const useHabitStore = create((set, get) => ({
       }
     } catch (err) {
       console.log(err)
-      toast.error(err.response?.data?.error || i18next.t("habit_update_failed"))
+      toast.error(err.response?.data?.message || i18next.t("habit_update_failed"))
       set(state => ({
         habits: state.habits.map(h =>
           h._id === id ? { ...h, completed: prevState } : h
