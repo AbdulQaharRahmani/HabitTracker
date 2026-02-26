4121235b40b2edb2358e5ad61fc7ff59b561b9e1
fataneh-mow <fataneh.mow@gmail.com>
2026-02-26 11:03:02 +0430

    feat: translated category icons #319

--- frontend/src/components/tasks/AddCategory.jsx
+++ frontend/src/components/tasks/AddCategory.jsx
@@ -124,16 +124,17 @@ export default function AddCategory() {
                                     <div className="flex gap-2 overflow-x-auto pb-3 mb-3 scrollbar-hide mx-2.5 mt-2">
                                         {categories.map((cat) => (
                                             <button
-                                            key={cat}
-                                            onClick={() => setSelectedCategory(cat)}
-                                            className={`px-4 py-2 rounded-full text-sm font-medium transition-all
-                                                ${
-                                                selectedCategory === cat
-                                                    ? "bg-indigo-500 text-white"
-                                                    : "bg-gray-100 dark:bg-gray-800 text-gray-600"
-                                                }`}
+                                                key={cat}
+                                                onClick={() => setSelectedCategory(cat)}
+                                                className={`px-4 py-2 rounded-full text-sm font-medium transition-all
+                                                    ${
+                                                    selectedCategory === cat
+                                                        ? "bg-indigo-500 text-white"
+                                                        : "bg-gray-100 dark:bg-gray-800 text-gray-600"
+                                                    }`
+                                                }
                                             >
-                                                {cat}
+                                                {t(`categoryGroups.${cat}`)}
                                             </button>
                                         ))}
                                     </div>
