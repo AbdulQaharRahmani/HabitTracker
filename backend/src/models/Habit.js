ba7c9ef3ae211c9fd95a5fa0e58c69b869dac3b7
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-02-27 16:21:13 +0430

    CHange the sort field by createdAt #372

--- backend/src/models/Habit.js
+++ backend/src/models/Habit.js
@@ -53,10 +53,14 @@ HabitSchema.index({ userId: 1, isDeleted: 1, order: 1 });
 HabitSchema.index({ userId: 1, title: 1 }, { unique: true });
 
 //Return the query object that can be awaited
-HabitSchema.statics.findByUserAndSortByOrder = function (skip, limit, query) {
+HabitSchema.statics.findByUserAndSortByCreationTime = function (
+  skip,
+  limit,
+  query
+) {
   return this.find(query)
     .populate('categoryId', 'name icon backgroundColor')
-    .sort({ order: 1 })
+    .sort({ createdAt: -1 })
     .skip(skip)
     .limit(limit)
     .lean();
